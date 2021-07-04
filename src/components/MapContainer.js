9700111291d198966c21c207e2ae6ff0eb4670f6
Shayan Nathan <shy@Shayans-MacBook-Pro.local>
2021-07-04 14:56:21 -0700

    Added map

---
+++ src/components/MapContainer.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
+import config from "../config";
+
+//May need to not hard-code styling
+const mapStyles = {
+  //width: '1110px',
+  //height: '79%'
+};
+
+export class MapContainer extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Map 
+        google={this.props.google}
+        zoom={9}
+        style={mapStyles}
+        initialCenter={
+          {
+            lat: 37.65,
+            lng: -122.33
+          }
+        }
+      >
+      </Map>
+    );
+  }
+
+  /* Put this before the </Map> tag
+  {this.props.data.map((trip, i) => {
+        return(<Marker
+          key={i}
+          position = {{
+            lat: trip.lat,
+            lng: trip.lng
+          }}
+          title = {trip.location}
+        />)
+      })}
+  */
+}
+
+export default GoogleApiWrapper({
+    apiKey: config.googleMapsAPIKey
+})(MapContainer);
